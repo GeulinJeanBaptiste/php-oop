67c463dbe60694c22a379a7c639ee31d78e0cf94
Geulin Jean-Baptiste <jeanbaptiste.geulin@gmail.com>
2023-12-04 15:56:16 +0100

    class statique

--- MaClass.php
+++ MaClass.php
@@ -1,6 +1,8 @@
 <?php
 class MaClass
 {
+
+    // public
     public string $attributPublic;
     public string $couleur = 'vert';
     public int $age = 54;
@@ -10,8 +12,17 @@ class MaClass
     public int $rayon;
 
 
+    static string $varStatic = 'Je suis une variable statique';
+
+    // Private
     private string $_attributPrivate;
     private string $_nom = 'Michel Plik';
+
+    // La méthode __construct est appelée automatiquement lorsqu'une nouvelle instance d'une classe est créée. C'est souvent utilisé pour effectuer des initialisations nécessaires à l'objet.
+    public function __construct()
+    {
+        echo 'Je suis une methode magique __construct de la class' . __CLASS__ . "<br>";
+    }
 
     public function displayMethode($value): string
     {
@@ -20,18 +31,40 @@ class MaClass
         return $this->prenom . ' aime la couleur ' . $this->couleur . ' ' . $value;
     }
 
+    // 1er methode pour calculer l'air d'un cercle.
     public function calculCercleAir()
     {
         // Les :: servent à appeler la constante.
         // echo self::PI;
         return self::PI * $this->rayon * $this->rayon;
     }
+
+    // 2e methode pour calculer l'air d'un cercle.
     public function calculCercleAir2($rayonParam)
     {
         // Les :: servent à appeler la constante.
         return self::PI * $rayonParam * $rayonParam;
     }
+
+    // La méthode __destruct est appelée automatiquement lorsqu'une instance d'une classe est détruite, c'est-à-dire lorsque l'objet n'est plus référencé ou lorsque la fonction unset() est utilisée pour détruire l'objet.
+    public function __destruct()
+    {
+        echo 'Je suis une methode magique (__destruct) de la class' . __CLASS__ . "<br>";
+    }
+
+
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    static function staticFunc()
+    {
+        return 'Methode statique qui affiche une variable statique ' . self::$varStatic;
+    }
 }
+
+
 $objClass = new MaClass();
 // echo $obj->couleur . '<br>';
 // echo $obj->age . '<br>';
@@ -39,7 +72,14 @@ $objClass = new MaClass();
 // echo $obj->nom . '<br>';
 // $objClass->prenom = 'Bill';
 // echo $objClass->displayMethode('');
+
+
+// 1er methode pour calculer l'air d'un cercle.
 $objClass->rayon = 3.5;
 echo $objClass->calculCercleAir() . 'cm2' .  "<br>";
 
+
+// 2e methode pour calculer l'air d'un cercle.
 echo $objClass->calculCercleAir2(5) . 'cm2' .  "<br>";
+
+echo maClass::$varStatic;
